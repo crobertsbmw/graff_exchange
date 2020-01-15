a67a9976c67b356ff794c8e39b4df8e40a70ff3a
Chase Roberts <crobertsbmw@gmail.com>
2020-01-15 19:45:49 +0000

    Updating admin

--- exchange/admin.py
+++ exchange/admin.py
@@ -6,7 +6,16 @@ from exchange.models import User, Sketch, Assignment, Exchange
 class UserAdmin(admin.ModelAdmin):
     list_display = ('email', 'moniker', 'level', 'write_style', 'recieve_style','do_double', 'comments', 'country', 'city')
 
+class SketchAdmin(admin.ModelAdmin):
+    list_display = ('user', 'get_recipient', 'exchange')
+    def get_recipient(self, obj):
+        return obj.assignment.recipient.moniker
+    get_recipient.short_description = 'Recipient'
+    get_recipient.admin_order_field = 'assignment__recipient__moniker'
+
+    
+
 admin.site.register(User, UserAdmin)
-admin.site.register(Sketch)
+admin.site.register(Sketch, SketchAdmin)
 admin.site.register(Assignment)
 admin.site.register(Exchange)
