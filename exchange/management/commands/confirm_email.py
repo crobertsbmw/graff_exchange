11df9b12047553c72cc4d4b146cfece8816f45ba
Chase Roberts <crobertsbmw@gmail.com>
2020-03-06 21:18:55 -0700

    Updating landing page

--- exchange/management/commands/confirm_email.py
+++ exchange/management/commands/confirm_email.py
@@ -8,7 +8,18 @@ Thanks again for signing up for the graff exchange! I just need you to confirm y
 
 {link}
 
-You can also edit your details if you need to. I'm hoping to get this underway this {date}, so you can expect an email then with your assignment.
+You can also edit your details if you need to. I'm hoping to get this underway this on {date}, so you can expect an email then with your assignment.
+
+Thanks,
+Chase
+'''
+
+message2 = '''{first_name},
+Hey, we're starting the next Exchange this {date}, if you want to join this month, go ahead and click this link and you can confirm your details.
+
+{link}
+
+Let me know if you have any questions or concerns.
 
 Thanks,
 Chase
@@ -20,10 +31,26 @@ def send_confirmation_email():
         m = message1.replace('{first_name}', user.name())
         m = m.replace('{link}', user.confirm_link())
         print('*****')
-        print("sending to ", assignment.user_signup.user.email)
+        print("sending to ", user.email)
         print(m)
-        email = EmailMessage('Graff Exchange Assignment', m, to=[assignment.user_signup.user.email])
+        email = EmailMessage('Confirm Graff Exchange Signup', m, to=[user.email])
         email.send()
+
+    two_months_ago = datetime.datetime.now() - datetime.timedelta(days=70)
+    sketches = Sketch.objects.filter(datetime__gt=two_months_ago)
+    user_pks = list(set([s.user_id for s in sketches]))
+    users = User.objects.filter(user_pk__in=user_pks).exclude(user_pk__in=[u.id for u in users])
+    
+    message = message1.replace('{date}', Exchange.this_month().assignment_date.strftime('%A'))
+    for user in users:
+        m = message1.replace('{first_name}', user.name())
+        m = m.replace('{link}', user.confirm_link())
+        print('*****')
+        print("sending to ", user.email)
+        print(m)
+        email = EmailMessage(datetime.datetime.now().strftime("%b Exchange?"), m, to=[user.email])
+        email.send()
+
 
 class Command(BaseCommand):
     def handle(self, *args, **options):
